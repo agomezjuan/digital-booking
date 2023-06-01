4df940c6818f42aa713f9eed6090808f33b8e84e
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-06-01 23:20:44 +0200

    feat(frontend): :sparkles: added validation to register form

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faFacebookF,
@@ -8,49 +9,72 @@ import {
 import './Register.css';
 
 const Register = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [isRegistered, setIsRegistered] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: '',
+      lastname: '',
+      email: '',
+      password: '',
+    },
+  });
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    if (isLoading) return;
-
-    setIsLoading(true);
-
-    // Simulate registration process
-    setTimeout(() => {
-      setIsLoading(false);
-      setIsRegistered(true);
-    }, 3000);
-  };
+  const onSubmit = (data) => console.log(data);
 
   return (
     <div className='register-body'>
       <div className='wrapper'>
         <form
-          className={`register ${isRegistered ? 'ok' : ''}`}
-          onSubmit={handleFormSubmit}
+          className={`register`} // className={`register ${isRegistered ? 'ok' : ''}`}
+          onSubmit={handleSubmit(onSubmit)}
         >
           <p className='register-title'>Registro</p>
           <div className='register-input-wrapper'>
-            <input type='text' placeholder='Nombre' autoFocus />
+            <input
+              type='text'
+              placeholder='Nombre'
+              autoFocus
+              {...register('name', { required: true })}
+            />
+            {errors.name && <span>Este campo es requerido</span>}
             <i className='fa fa-user'></i>
           </div>
           <div className='register-input-wrapper'>
-            <input type='text' placeholder='Apellido' />
+            <input
+              type='text'
+              placeholder='Apellido'
+              {...register('lastname', { required: true })}
+            />
+            {errors.name && <span>Este campo es requerido</span>}
             <i className='fa fa-user'></i>
           </div>
           <div className='register-input-wrapper'>
-            <input type='email' placeholder='Correo electrónico' />
+            <input
+              type='email'
+              placeholder='Correo electrónico'
+              {...register('email', { required: true })}
+            />
+            {errors.name && <span>Este campo es requerido</span>}
             <i className='fa fa-envelope'></i>
           </div>
           <div className='register-input-wrapper'>
-            <input type='password' placeholder='Contraseña' />
+            <input
+              type='password'
+              placeholder='Contraseña'
+              {...register('password', { required: true })}
+            />
+            {errors.name && <span>Este campo es requerido</span>}
             <i className='fa fa-key'></i>
           </div>
           <button type='submit' className='register-button'>
             Registrarse
           </button>
+          <p>
+            ¿Ya tienes cuenta? <Link to='/login'>Iniciar Sesion</Link>
+          </p>
         </form>
         <footer className='register-footer'>
           <div className='register-social-icons'>
