660e6b83f9f68ff44f747e9325f1d13785f53430
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-06-21 23:15:10 +0200

    feat(frontend): :sparkles: Geocoded map and features

--- frontend/src/components/HotelFeatures/HotelFeatures.jsx
+++ frontend/src/components/HotelFeatures/HotelFeatures.jsx
@@ -1,21 +1,31 @@
 import './HotelFeatures.scss';
 import PropTypes from 'prop-types';
+import { features as allFeatures } from '../../mocks/features';
 
 function HotelFeatures({ features }) {
   return (
     <div className='ContainerProductFeatures'>
       <div className='ContainerTitleFeatures'>
-        <h2>¿Qué ofrece este lugar?</h2>
+        <div className='container'>
+          <h2>¿Qué ofrece este lugar?</h2>
+        </div>
       </div>
-      <div className='ContainerFeaturesAndIcons'>
-        {features.map((feature) => {
-          return (
-            <div key={feature.id}>
-              <i className={feature.icon}></i>
-              <p>{feature.name}</p>
-            </div>
-          );
-        })}
+      <div className='container'>
+        <div className='ContainerFeaturesAndIcons'>
+          {features.map((feature) => {
+            const featRender = allFeatures.find((f) => f.name === feature);
+            return (
+              <div className='hotel-feature' key={featRender.name}>
+                <div>
+                  <ion-icon name={featRender.icon}></ion-icon>
+                  <span>{featRender.name}</span>
+                </div>
+
+                <p>{featRender.description}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
